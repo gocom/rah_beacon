dacc13420586e772bf57c1b6fcb6f9f8d1980b88
Jukka Svahn <web@rahinaa.biz>
2012-11-14 22:35:57 +0200

    Programming style.

--- rah_beacon.php
+++ rah_beacon.php
@@ -3,85 +3,97 @@
 /**
  * Rah_beacon plugin for Textpattern CMS.
  *
- * @author Jukka Svahn
- * @date 2012-
+ * @author  Jukka Svahn
+ * @date    2012-
  * @license GNU GPLv2
- * @link https://github.com/gocom/rah_beacon
+ * @link    https://github.com/gocom/rah_beacon
  * 
- * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
+ * Copyright (C) 2012 Jukka Svahn http://rahforum.biz
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 	new rah_beacon();
 
-class rah_beacon {
+/**
+ * The plugin class.
+ */
 
+class rah_beacon
+{
 	/**
-	 * Constructor
+	 * Constructor.
 	 */
-	
-	public function __construct() {
+
+	public function __construct()
+	{
 		register_callback(array($this, 'light'), 'pretext');
 	}
-	
+
 	/**
-	 * Registers forms as tags
+	 * Registers forms as tags.
 	 */
-	
-	public function light() {
-		
+
+	public function light()
+	{	
 		$forms = 
 			safe_column(
 				'name',
 				'txp_form',
 				'1=1'
 			);
-		
+
 		$beacon = new rah_beacons();
-		
-		foreach($forms as $name) {
-			if(!preg_match('/^[a-z_][a-z0-9_]*$/', $name)) {
+
+		foreach ($forms as $name)
+		{
+			if (!preg_match('/^[a-z_][a-z0-9_]*$/', $name))
+			{
 				trace_add('[rah_beacon: '.$name.' skipped]');
 				continue;
 			}
-			
+
 			$token = token_get_all('<?php function '.$name.'(){} ?>');
-			
-			if(isset($token[3][0]) && $token[3][0] !== T_STRING) {
+
+			if (isset($token[3][0]) && $token[3][0] !== T_STRING)
+			{
 				trace_add('[rah_beacon: '.$name.' skipped]');
 				continue;
 			}
-			
+
 			$beacon->$name();
 		}
 	}
-	
+
 	/**
-	 * Handles tag calls
+	 * Handles tag calls.
 	 */
-	
-	static public function operator($alias, $atts, $thing=null) {
+
+	static public function operator($alias, $atts, $thing = null)
+	{
 		global $variable;
-		
+
 		$original = (array) $variable;
-		
-		if($thing !== null) {
+
+		if ($thing !== null)
+		{
 			$atts['thing'] = $atts['true'] = parse(EvalElse($thing, true));
 			$atts['false'] = parse(EvalElse($thing, false));
 		}
-		
+
 		$variable = array_merge($original, $atts);
 		$out = output_form(array('form' => $alias), $thing);
-		
-		foreach($atts as $name) {
+
+		foreach ($atts as $name)
+		{
 			unset($variable[$name]);
-			
-			if(isset($original[$name])) {
+
+			if (isset($original[$name]))
+			{
 				$variable[$name] = $original[$name];
 			}
 		}
-		
+
 		return $out;
 	}
 }
@@ -90,16 +102,26 @@ class rah_beacon {
  * Creates lighthouse members
  */
 
-class rah_beacons {
-	public function __call($name, $arguments) {
-	
-		if(function_exists($name)) {
+class rah_beacons
+{
+	/**
+	 * Registers a new tag handler function.
+	 *
+	 * @param  string $name      Tag name
+	 * @param  string $arguments Not used
+	 * @return bool   FALSE on error
+	 */
+
+	public function __call($name, $arguments)
+	{
+		if (function_exists($name))
+		{
 			trace_add('[rah_beacon: <txp:'.$name.' /> already reserved]');
 			return false;
 		}
-		
+
 		trace_add('[rah_beacon: <txp:'.$name.' /> created]');
-		
+
 		eval(<<<EOF
 			function {$name}(\$atts, \$thing) {
 				return rah_beacon::operator(__FUNCTION__, \$atts, \$thing);

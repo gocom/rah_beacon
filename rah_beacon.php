581633cf44f04dc6dcf1d0b8031aad7c468fdb74
Jukka Svahn <web@rahinaa.biz>
2013-06-20 21:01:15 +0300

    Updated the header.

--- rah_beacon.php
+++ rah_beacon.php
@@ -4,12 +4,11 @@
  * Rah_beacon plugin for Textpattern CMS.
  *
  * @author  Jukka Svahn
- * @date    2012-
  * @license GNU GPLv2
  * @link    https://github.com/gocom/rah_beacon
  * 
- * Copyright (C) 2012 Jukka Svahn http://rahforum.biz
- * Licensed under GNU Genral Public License version 2
+ * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
+ * Licensed under GNU General Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 

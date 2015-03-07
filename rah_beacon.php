fef8d88a6eda154e0dd2f813d00d76105f94e3a1
Jukka Svahn <web@rahinaa.biz>
2015-03-07 18:41:52 +0200

    Updated header.

--- rah_beacon.php
+++ rah_beacon.php
@@ -1,15 +1,24 @@
 <?php
 
-/**
- * Rah_beacon plugin for Textpattern CMS.
+/*
+ * rah_beacon - Flat templates for Textpattern CMS
+ * https://github.com/gocom/rah_beacon
  *
- * @author  Jukka Svahn
- * @license GNU GPLv2
- * @link    https://github.com/gocom/rah_beacon
- * 
- * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
- * Licensed under GNU General Public License version 2
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2015 Jukka Svahn
+ *
+ * This file is part of rah_beacon.
+ *
+ * rah_beacon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_beacon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_beacon. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
